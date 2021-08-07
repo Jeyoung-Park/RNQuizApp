23d90a328150b4767b42fc87b0dbc0e63e9f1d2c
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-08-07 22:07:38 +0900

    chore: 프로젝트 절대경로 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  alias: {
+    '@': './src',
+    '@components': './src/components',
+    '@containers': './src/containers',
+    '@modules': './src/modules',
+    '@scenes': './src/scenes',
+    '@utils': './src/utils',
+    '@env': '.src/config/env',
+  },
 };
