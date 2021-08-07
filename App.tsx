659c7c8e77f26e50b726a60102d1deabac713d67
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-08-07 14:12:47 +0900

    chore: prettier 설정

--- App.tsx
+++ App.tsx
@@ -9,21 +9,21 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 const App = () => {
-  return (
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    <SafeAreaView style={styles.container}>
-      <Text>Quiz App</Text>
-    </SafeAreaView>
-  );
+	return (
+		// eslint-disable-next-line @typescript-eslint/no-use-before-define
+		<SafeAreaView style={styles.container}>
+			<Text>Quiz App</Text>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+	container: {
+		flex: 1,
+	},
 });
 
 export default App;
