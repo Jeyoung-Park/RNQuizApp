80e95bbd7a4bf0d69276a320326e2eae37b30ccd
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-08-11 20:58:21 +0900

    style: Home/index prettier 적용

--- src/components/screen/Home/index.tsx
+++ src/components/screen/Home/index.tsx
@@ -1,5 +1,13 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import * as React from 'react';
-import { Text, View, StyleSheet, Button, TouchableOpacity, SafeAreaView } from 'react-native';
+import {
+	Text,
+	View,
+	StyleSheet,
+	Button,
+	TouchableOpacity,
+	SafeAreaView,
+} from 'react-native';
 
 interface HomeProps {
 	// route:any,
@@ -7,30 +15,28 @@ interface HomeProps {
 }
 
 const Home = ({ navigation }: HomeProps) => {
-	const goToQuiz=() => {
+	const goToQuiz = () => {
 		// console.log("퀴즈 풀기 버튼 클릭");
 		navigation.navigate('Quiz');
 	};
-	return(
+	return (
 		// eslint-disable-next-line @typescript-eslint/no-use-before-define
 		<SafeAreaView style={styles.container}>
 			<View style={styles.textContainer}>
 				<Text style={styles.titleText}>React Native Quiz App</Text>
-				<Text style={styles.descriptionText}>React Native로 제작된 퀴즈 앱입니다.</Text>
+				<Text style={styles.descriptionText}>
+					React Native로 제작된 퀴즈 앱입니다.
+				</Text>
 			</View>
-			
+
 			<View style={styles.buttonContainer}>
-				<TouchableOpacity	
-					style={styles.quizButton}
-					onPress={goToQuiz}
-				>
-				<Text style={styles.quizButtonText}>퀴즈 풀기</Text>
+				<TouchableOpacity style={styles.quizButton} onPress={goToQuiz}>
+					<Text style={styles.quizButtonText}>퀴즈 풀기</Text>
 				</TouchableOpacity>
 			</View>
-			
 		</SafeAreaView>
 	);
-}
+};
 
 export default Home;
 
@@ -38,39 +44,37 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		// justifyContent: 'space-around',
-		alignItems:'center',
+		alignItems: 'center',
 	},
-	textContainer:{
-		flex:1,
+	textContainer: {
+		flex: 1,
 		justifyContent: 'center',
-		alignItems:'center',
+		alignItems: 'center',
 	},
-	titleText:{fontSize:32,},
-	descriptionText:{
-		fontSize:20,
-		marginTop:20,
+	titleText: { fontSize: 32 },
+	descriptionText: {
+		fontSize: 20,
+		marginTop: 20,
 	},
-	buttonContainer:{
-		flex:1,
+	buttonContainer: {
+		flex: 1,
 		// backgroundColor:'red',
-		width:'100%',
+		width: '100%',
 		justifyContent: 'center',
-		alignItems:'center',
+		alignItems: 'center',
 	},
-	quizButton:{
+	quizButton: {
+		width: '80%',
+		paddingVertical: 20,
 
-		width:'80%',
-		paddingVertical:20,
+		backgroundColor: 'gray',
 
-		backgroundColor:'gray',
+		justifyContent: 'center',
+		alignItems: 'center',
 
-		justifyContent:'center',
-		alignItems:'center',
-
-		borderRadius:20,
-
+		borderRadius: 20,
 	},
-	quizButtonText:{
-		fontSize:16,
-	}
+	quizButtonText: {
+		fontSize: 16,
+	},
 });
