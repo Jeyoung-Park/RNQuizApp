8f9ccb73d6da8b382d6022cdadfcc8eb66d006fe
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-08-12 22:13:35 +0900

    refactor: redux wrongQuiz.ts 분리

--- src/models/quiz.ts
+++ src/models/quiz.ts
@@ -13,8 +13,6 @@ export const initialState = {
 	correctNumber: null,
 	// 재도전 횟수
 	retryCount: 0,
-	// 오답노트 목록
-	wrongQuizList: [],
 };
 
 // 액션 정의
@@ -35,9 +33,6 @@ const SET_QUIZ_CORRECT_NUMBER = 'quiz/SET_QUIZ_CORRECT_NUMBER';
 
 // 퀴즈 retry 횟수 set하기
 const SET_RETRY_COUNT = 'quiz/SET_RETRY_COUNT';
-
-// 오답노트 목록 set하기
-const SET_WRONG_QUIZ_LIST = 'quiz/SET_WRONG_QUIZ_LIST';
 
 // *********************************** thunk ************************************
 //  비동기 액션일 때는 createPromiseThunk 호출, 아니면 그냥 action(object) 리턴
@@ -62,11 +57,6 @@ export const setQuizCorrectNumberAction = (quizCorrectNumber: number) => ({
 export const setRetryCountAction = (retryCount: number) => ({
 	type: SET_RETRY_COUNT,
 	payload: retryCount,
-});
-
-export const setWrongQuizListAction = (wrongQuizList: any) => ({
-	type: SET_WRONG_QUIZ_LIST,
-	payload: wrongQuizList,
 });
 
 // *********************************** reducer ***********************************
@@ -98,14 +88,6 @@ export default handleActions(
 		) => ({
 			...state,
 			retryCount: result,
-		}),
-
-		[SET_WRONG_QUIZ_LIST]: (
-			state: any,
-			{ payload: result }: { payload: number },
-		) => ({
-			...state,
-			wrongQuizList: result,
 		}),
 	},
 	initialState,
