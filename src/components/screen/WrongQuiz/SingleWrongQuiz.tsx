5ce4644e20f88f274f9e9060db67939e2874c49c
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-08-18 00:52:45 +0900

    fix: 오답노트 상세 페이지 뒤로가기 버튼 누락
    -오답 노트 상세 페이지 뒤로가기 버튼 누락되어 있는 것 추가

--- src/components/screen/WrongQuiz/SingleWrongQuiz.tsx
+++ src/components/screen/WrongQuiz/SingleWrongQuiz.tsx
@@ -20,7 +20,7 @@ const SingleWrongQuiz = ({ navigation, route }: SingleWrongQuizProps) => {
 	return (
 		<View style={styles.container}>
 			{/* <Text>SingleWrongQuiz</Text> */}
-			<Header title="오답노트 상세" goBackText="Go Back" goBack={goBack} />
+			<Header title="오답노트 상세" goBackIcon="left" goBack={goBack} />
 			<View style={styles.singleQuizContainer}>
 				<SingleQuiz
 					currentQuiz={quizItem}
